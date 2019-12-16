b8ec485f7d5dac0a17e96cfc4942121bbb00b8ae
Mohd Nadeem <mohd.nadeem@cars24.com>
2019-12-16 15:37:08 +0530

    linked list | remove duplicates from sorted list

--- src/linkedlist/RemoveDuplicates.java
+++ src/linkedlist/RemoveDuplicates.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import utils.AssortedMethods;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,18 +12,42 @@ import java.util.Set;
  */
 public class RemoveDuplicates {
 
+    public static void main(String[] args) {
+        LinkedListNode list = AssortedMethods.createLinkedListFromArray(new int[]{1, 2, 2, 2, 3, 4, 4, 5});
+        System.out.println("list = " + list.printForward());
+
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        LinkedListNode head = removeDuplicates.removeDuplicatesFromSortedList(list);
+        System.out.println("head = " + head.printForward());
+    }
+
     public void removeDuplicates(LinkedListNode head){
         Set<Integer> uniques = new HashSet<>();
         while(head != null){
             if(uniques.contains(head.data)){
                 if(head.next != null){
-                    head.next.setPrevious(head.prev);
+                    head.next.prev = head.prev;
                 }
-                head.prev.setNext(head.next);
+                head.prev.next = head.next;
             }else{
                 uniques.add(head.data);
             }
             head = head.next;
         }
+    }
+
+    public LinkedListNode removeDuplicatesFromSortedList(LinkedListNode head){
+        LinkedListNode pre = null;
+        LinkedListNode curr = head;
+        while(curr != null){
+            LinkedListNode next = curr.next;
+            if(pre != null && pre.data == curr.data){
+                pre.next = next;
+            } else {
+                pre = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
     }
 }
