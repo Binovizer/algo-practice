36164337f126075226dbd0c11b3d61cb7a5bd0b4
Mohd Nadeem <mohd.nadeem@cars24.com>
2019-12-19 03:07:51 +0530

    stack | stack using two queues

--- src/stack/StackUsingTwoQueues.java
+++ src/stack/StackUsingTwoQueues.java
@@ -1,27 +1,55 @@
 package stack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * The type StackUsingTwoQueues
+ * The type StackUsingQueues
  *
  * @author Mohd Nadeem
  */
 public class StackUsingTwoQueues {
 
-    public static void main(String[] args) {
-        StackUsingQueues stackUsingQueues = new StackUsingQueues();
-        stackUsingQueues.push(1);
-        stackUsingQueues.push(2);
-        stackUsingQueues.push(3);
-        Integer pop = stackUsingQueues.pop();
-        System.out.println("pop1 = " + pop);
-        stackUsingQueues.push(4);
-        stackUsingQueues.push(5);
-        System.out.println("pop2 = " + stackUsingQueues.pop());
-        System.out.println("pop2 = " + stackUsingQueues.pop());
-        System.out.println("pop2 = " + stackUsingQueues.pop());
-        System.out.println("pop2 = " + stackUsingQueues.pop());
+    private final Queue<Integer> q1;
+    private final Queue<Integer> q2;
 
+    public StackUsingTwoQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
 
+    public void push(Integer num) {
+        while(!q1.isEmpty()){
+            Integer poll = q1.poll();
+            q2.offer(poll);
+        }
+        q1.offer(num);
+        while(!q2.isEmpty()){
+            Integer poll = q2.poll();
+            q1.offer(poll);
+        }
+    }
 
+    public Integer pop() {
+        if(q1.isEmpty()){
+            return -1;
+        }
+        return q1.poll();
+    }
+
+    public static void main(String[] args) {
+        StackUsingTwoQueues stackUsingTwoQueues = new StackUsingTwoQueues();
+        stackUsingTwoQueues.push(1);
+        stackUsingTwoQueues.push(2);
+        stackUsingTwoQueues.push(3);
+        Integer pop = stackUsingTwoQueues.pop();
+        System.out.println("pop1 = " + pop);
+        stackUsingTwoQueues.push(4);
+        stackUsingTwoQueues.push(5);
+        System.out.println("pop2 = " + stackUsingTwoQueues.pop());
+        System.out.println("pop2 = " + stackUsingTwoQueues.pop());
+        System.out.println("pop2 = " + stackUsingTwoQueues.pop());
+        System.out.println("pop2 = " + stackUsingTwoQueues.pop());
+
+    }
 }
