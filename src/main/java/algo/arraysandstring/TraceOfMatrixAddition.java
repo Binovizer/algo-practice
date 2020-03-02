516cf99c99453c749d08c70d124b21bdc72a663c
Mohd Nadeem <mohd.nadeem3464@gmail.com>
2020-03-02 22:05:28 +0530

    Array | convert to zig zag array

--- src/main/java/algo/arraysandstring/TraceOfMatrixAddition.java
+++ src/main/java/algo/arraysandstring/TraceOfMatrixAddition.java
@@ -4,7 +4,7 @@ public class TraceOfMatrixAddition {
 
     public static void main(String[] args) {
         int m = 3;
-        int n = 4;
+        int n = 3;
         TraceOfMatrixAddition traceOfMatrixAddition = new TraceOfMatrixAddition();
         int trace = traceOfMatrixAddition.findTrace(m, n);
         System.out.println("trace = " + trace);
@@ -13,14 +13,12 @@ public class TraceOfMatrixAddition {
     public int findTrace(int m, int n) {
         int trace = 0;
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    int rowValue = (n * i + j + 1);
-                    System.out.println(i + ", " + j + " : " + rowValue);
-                    int colValue = (m * i + j + 1);
-                    System.out.println(i + ", " + j + " : " + colValue);
-                    trace += rowValue + colValue;
-                }
+            if (i < n) {
+                int rowValue = i * (n + 1) + 1;
+//                    System.out.println(i + ", " + j + " : " + rowValue);
+                int colValue = i * (m + 1) + 1;
+//                    System.out.println(i + ", " + j + " : " + colValue);
+                trace += rowValue + colValue;
             }
         }
         return trace;
---
+++ src/main/java/algo/recursion/SumOfDigits.java
@@ -0,0 +1,18 @@
+package algo.recursion;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+        int num = 12345;
+        SumOfDigits sumOfDigits = new SumOfDigits();
+        int sum = sumOfDigits.sum(num);
+        System.out.println("sum = " + sum);
+    }
+
+    private int sum(int num) {
+        if (num < 10) {
+            return num;
+        }
+        return sum(num / 10) + (num % 10);
+    }
+}
