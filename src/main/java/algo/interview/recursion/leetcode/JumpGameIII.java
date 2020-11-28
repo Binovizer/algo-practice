6bbcee6bc399509a24c97ae452ff9630d797dd1f
Nadeem <wb-mohdnadee758588@antgroup.com>
2020-11-28 20:25:34 +0530

    recursion | leetcode

---
+++ src/main/java/algo/interview/recursion/leetcode/JumpGameIII.java
@@ -0,0 +1,38 @@
+package algo.interview.recursion.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Problem Desc : https://leetcode.com/problems/jump-game-iii/
+ *
+ * @author Nadeem 2020-11-28
+ */
+public class JumpGameIII {
+
+    public static void main(String[] args) {
+        int[] arr = {3,0,2,1,2};
+        int start = 2;
+
+        JumpGameIII jumpGameIII = new JumpGameIII();
+        boolean canReach = jumpGameIII.canReach(arr, start);
+        System.out.println("canReach = " + canReach);
+    }
+
+    public boolean canReach(int[] arr, int start) {
+        Set<Integer> traversedIndices = new HashSet<>();
+        return canReach(arr, start, traversedIndices);
+    }
+
+    private boolean canReach(int[] arr, int index, Set<Integer> traversedIndices) {
+        if (index < 0 || index >= arr.length || traversedIndices.contains(index)) {
+            return false;
+        }
+        if(arr[index] == 0){
+            return true;
+        }
+        traversedIndices.add(index);
+        return canReach(arr, index + arr[index], traversedIndices)
+                || canReach(arr, index - arr[index], traversedIndices);
+    }
+}
