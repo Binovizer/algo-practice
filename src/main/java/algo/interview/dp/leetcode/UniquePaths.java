7875e3e6a4beb4f20c5acd3964840186636de6cc
Nadeem <wb-mohdnadee758588@antgroup.com>
2020-12-10 02:18:42 +0530

    dp | unique paths

--- src/main/java/algo/interview/dp/leetcode/UniquePaths.java
+++ src/main/java/algo/interview/dp/leetcode/UniquePaths.java
@@ -13,7 +13,7 @@ public class UniquePaths {
 
     public static void main(String[] args) {
         UniquePaths uniquePaths = new UniquePaths();
-        int paths = uniquePaths.uniquePaths(3, 7);
+        int paths = uniquePaths.uniquePathsBottomUp(3, 7);
         System.out.println("paths = " + paths);
     }
 
@@ -46,16 +46,18 @@ public class UniquePaths {
         return dp[i][j] = uniquePathsMemo(i + 1, j, m, n) + uniquePathsMemo(i, j + 1, m, n);
     }
 
-    private int uniquePathsBottomUp(int i, int j, int m, int n) {
-        if (i >= m || j >= n) {
-            return 0;
+    private int uniquePathsBottomUp(int m, int n) {
+        int[][] dp = new int[m][n];
+        // Initialization
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
         }
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        return dp[i][j] = uniquePathsMemo(i + 1, j, m, n) + uniquePathsMemo(i, j + 1, m, n);
+        return dp[m - 1][n - 1];
     }
 }
