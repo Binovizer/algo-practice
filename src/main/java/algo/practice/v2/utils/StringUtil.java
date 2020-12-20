034ee17b04259aa7966cc1c0659ad7e1295f00ec
Nadeem <wb-mohdnadee758588@antgroup.com>
2020-12-20 23:56:18 +0530

    increasing triplets

---
+++ src/main/java/algo/practice/v2/utils/StringUtil.java
@@ -0,0 +1,13 @@
+package algo.practice.v2.utils;
+
+/**
+ * This is the description of what this type does
+ *
+ * @author Nadeem 2020-12-20
+ */
+public class StringUtil {
+
+    public static String repeatString(String str, int times) {
+        return new String(new char[times]).replace("\0", str);
+    }
+}
