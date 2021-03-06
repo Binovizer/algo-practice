9c4ac5856032ff96664f6ee3a303b4c121d159ac
Nadeem <wb-mohdnadee758588@antgroup.com>
2021-03-06 12:50:01 +0530

    general problems

---
+++ src/main/java/algo/practice/v2/platforms/leetcode/general/KClosestPoints.java
@@ -0,0 +1,48 @@
+package algo.practice.v2.platforms.leetcode.general;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode.com/problems/k-closest-points-to-origin/
+ *
+ * @author Nadeem 2021-03-06
+ */
+public class KClosestPoints {
+
+    public int[][] kClosest(int[][] points, int k) {
+        if (k >= points.length) {
+            return points;
+        }
+        PriorityQueue<Point> q = new PriorityQueue<>(Comparator.comparingInt(o -> o.d));
+        for (int[] point : points) {
+            int x = point[0];
+            int y = point[1];
+            Point p = new Point(x, y);
+            p.d = x * x + y * y;
+            q.add(p);
+        }
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            Point poll = q.poll();
+            ans[i][0] = poll.x;
+            ans[i][1] = poll.y;
+        }
+        return ans;
+    }
+
+    static class Point {
+        int x;
+        int y;
+        int d; // distance from origin
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void setD(int d) {
+            this.d = d;
+        }
+    }
+}
