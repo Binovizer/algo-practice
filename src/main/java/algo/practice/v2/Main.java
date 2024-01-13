7e0c473d80a562c22afef9c50818447ba6229b30
Nadeem <mohd.nadeem3464@gmail.com>
2024-01-13 20:55:32 +0530

    POM Fix

---
+++ src/main/java/algo/practice/v2/Main.java
@@ -0,0 +1,13 @@
+package algo.practice.v2;
+
+/**
+ * The type Main
+ *
+ * @author nadeem Date : 08/09/21
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
